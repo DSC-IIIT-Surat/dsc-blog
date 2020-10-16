d08fe7244936d7a77155f7b3f94c72dda05ed39d
Shreeansh Gupta <sarojvarshney20@gmail.com>
2020-10-16 20:31:22 +0530

    added database connection and user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    confirmPassword: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
