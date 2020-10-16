c77e653f9ea864e40528af283e87d7ceb0469ab1
Shreeansh Gupta <sarojvarshney20@gmail.com>
2020-10-16 21:21:25 +0530

    registration route complete for users

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -1,4 +1,5 @@
 module.exports = {
   port: 5000,
   mongodbURI: "mongodb://localhost:27017/blog-app",
+  jwtSecret: "secret-key-goes-here-for-dev",
 };
--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,4 +1,5 @@
 module.exports = {
   port: process.env.PORT,
   mongodbURI: process.env.MONGODB_URI,
+  jwtSecret: process.env.JWT_SECRET,
 };
